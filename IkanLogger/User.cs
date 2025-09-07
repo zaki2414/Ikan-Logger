d21b1ad4d4234e267b0437585f94524857b987ee
FaizArsyiP <faizpragata@gmail.com>
2025-09-07 15:34:42 +0700

    Membuat projek form sederhana

---
+++ IkanLogger/User.cs
@@ -0,0 +1,19 @@
+namespace IkanLogger
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public override string ToString()
+        {
+            return $"Username: {Username}, Password: {Password}";
+        }
+    }
+}
