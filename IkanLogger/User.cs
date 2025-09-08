177a3106e8cd048fe32d43a146aa087f95a145b2
FaizArsyiP <faizpragata@gmail.com>
2025-09-08 14:05:05 +0700

    Menambahkan class

--- IkanLogger/User.cs
+++ IkanLogger/User.cs
@@ -2,18 +2,38 @@ namespace IkanLogger
 {
     public class User
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
+        private int idUser;
+        private string nama;
+        private string password;
 
-        public User(string username, string password)
+     public int IdUser
         {
-            Username = username;
-            Password = password;
+            get { return idUser; }
+        }
+        public string Nama
+        {
+            get { return nama; }
+            set { nama = value; }
+        }
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
         }
 
-        public override string ToString()
+        public Boolean Login(string nama, string password)
         {
-            return $"Username: {Username}, Password: {Password}";
+            if (nama == "admin" && password == "admin")
+            {
+                this.idUser = 1;
+                this.nama = nama;
+                this.password = password;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
